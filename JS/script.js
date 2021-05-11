365aeb78a4abb88546ab1d6dba87561207bcc09d
Jmc20001 <200210028@estudantes.ips.pt>
2021-05-11 19:34:27 +0100

    Update script.js
    listar disciplinas

--- JS/script.js
+++ JS/script.js
@@ -40,7 +40,7 @@ class Aluno{
         this.foto = foto
     }
 
-    showAlunos(divid){
+    showAlunos(divid){ //Lista os alunos
         let divobj = document.getElementById(divid);
         while(divobj.firstChild){
             divobj.removeChild(divobj.firstChild);
@@ -69,7 +69,7 @@ class Aluno{
         var tbody = document.createElement("tbody");
         table.appendChild(tbody);
 
-        for(let alunos of this.alunos){
+        for(let alunos of this.aluno){
             var row = document.createElement("tr");
             tbody.appendChild(row);
             var td = document.createElement("td");
@@ -101,7 +101,47 @@ class Disciplina{
         this.id = id        
         this.nome = nome        
         this.docente = docente
-    }    
+    }
+    
+    showDisciplinas(divid){ //Lista as disciplinas
+        let divobj = document.getElementById(divid);
+        while(divobj.firstChild){
+            divobj.removeChild(divobj.firstChild);
+        }
+        var table = document.createElement("table");
+        table.className = "table";
+        var thead = document.createElement("thead");
+        table.append(thead);
+        var row = document.createElement("tr");
+        thead.appendChild(row);
+        var th = document.createElement("th");
+        row.appendChild(th);
+        th.appendChild(document.createTextNode("Id"));
+        th = document.createElement("th");
+        row.appendChild(th);
+        th.appendChild(document.createTextNode("Nome"));
+        th = document.createElement("th");
+        row.appendChild(th);
+        th.appendChild(document.createTextNode("Docente"));
+        var tbody = document.createElement("tbody");
+        table.appendChild(tbody);
+
+        for(let disciplinas of this.disciplina){
+            var row = document.createElement("tr");
+            tbody.appendChild(row);
+            var td = document.createElement("td");
+            row.appendChild(td);
+            td.appendChild(document.createTextNode(disciplinas.id));
+            td = document.createElement("td");
+            row.appendChild(td);
+            td.appendChild(document.createTextNode(disciplinas.nome));
+            td = document.createElement("td");
+            row.appendChild(td);
+            td.appendChild(document.createTextNode(disciplinas.docente));
+        }
+
+        divobj.appendChild(table);
+    }
 }
 
 class InscricaoDisc{
