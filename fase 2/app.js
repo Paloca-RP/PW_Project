12ab4bd6f32c1e4ee0c2b553f882b3a2f43daefa
Paloca-RP <ricardopalhoca@hotmail.com>
2021-06-17 11:43:18 +0100

    fase 2

---
+++ fase 2/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const requestHandlers = require("./scripts/request-handlers.js");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("www"));
+
+// People
+app.get("/person", requestHandlers.getPeople);
+
+// Countries
+/** @todo Completar */
+app.get("/country", requestHandlers.getCountries);
+
+app.listen(8081, function () {
+    console.log("Server running at http://localhost:8081");
+});
---
+++ fase 2/scripts/request-handlers.js
@@ -0,0 +1,10 @@
+const mysql = require("mysql");
+const options = require("./connection-options.json");
+
+/**
+ * Função para retornar a lista de pessoas da BD.
+ * @param {*} req 
+ * @param {*} res 
+ */
+
+//funçoes do programa
