e6cf4fb11bf6c6287b8de80620d9aa7427da3f0e
Ricardo Palhoça <73611099+Paloca-RP@users.noreply.github.com>
2021-06-28 13:13:27 +0100

    q

--- Project_Joelho_Titânio/www/JS/script.js
+++ Project_Joelho_Titânio/www/JS/script.js
@@ -7,14 +7,3 @@ function closeNav() {
     document.getElementById("navbar").style.width = "0%";
   }
 //---------------------------------------------------------------  
-$(document).ready(function(){
-    $("#myBtn").click(function(){
-      $("#myModal").modal({backdrop: true});
-    });
-    $("#myBtn2").click(function(){
-      $("#myModal2").modal({backdrop: true});
-    });
-    $("#myBtn3").click(function(){
-      $("#myModal3").modal({backdrop: true});
-    });
-  });
